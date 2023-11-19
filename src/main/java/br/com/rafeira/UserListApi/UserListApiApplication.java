8d36701d16eeee00831764f5b9a8b2c657e1fe6e
rafeira <rafael12wq@gmail.com>
2023-11-19 10:16:36 -0300

    initial commit

---
+++ src/main/java/br/com/rafeira/UserListApi/UserListApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafeira.UserListApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserListApiApplication.class, args);
+	}
+
+}
